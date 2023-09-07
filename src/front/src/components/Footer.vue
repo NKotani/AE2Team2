6ab5838e5c1d1e5b0c3d87fd5674a15eccfd6ca9
NKotani <nanami.kotani@star.rcast.u-tokyo.ac.jp>
2023-09-07 13:59:20 +0900

    footerのlinkとHeaderのマージエラー修正

--- src/front/src/components/Footer.vue
+++ src/front/src/components/Footer.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row class="d-flex text-center" align="center" justify="center">
         <div class="mr-5 subtitle-1" style="vertical-align: middle;">
-          &copy; 2023 Team-2 <img src="https://media.discordapp.net/attachments/1138049081864884277/1149146575285780540/icon.png?width=298&height=282" width="25" height="25" style="vertical-align: middle; margin-left: 5px;">
+          &copy; 2023 Team-2 <img src="../assets/icon.png" width="25" height="25" style="vertical-align: middle; margin-left: 5px;">
         </div>
       </v-row>
     </v-container>
