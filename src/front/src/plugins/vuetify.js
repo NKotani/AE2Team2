6b0c4a53145cef2b3a69bb416ae5beb42d7d29ea
NKotani <nanami.kotani@star.rcast.u-tokyo.ac.jp>
2023-09-07 11:54:54 +0900

    header雛形

--- src/front/src/plugins/vuetify.js
+++ src/front/src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+// import Vuetify, { colors } from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
@@ -8,4 +9,16 @@ export default new Vuetify({
   icons: {
     iconfont: 'fa',
   },
+  // テーマカラー設定したかったけど諦めました
+  // theme: {
+  //   themes: {
+  //     light: {
+  //       dark: false,
+  //       colors: {
+  //         primaryOrange: '#FA6946',
+  //         secondaryGray: colors.grey.lighten2,
+  //       },
+  //     },
+  //   },
+  // },
 });
